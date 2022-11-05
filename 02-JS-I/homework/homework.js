229b50fd625c00c8a999e008d82ae9a16208ff0c
agusbusta <agusbus95@gmail.com>
2022-11-05 13:57:35 -0300

    finished

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -12,7 +12,7 @@ const nuevoBool = true;
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
 
-// Resuelve el siguiente problema matemático:
+//  Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
